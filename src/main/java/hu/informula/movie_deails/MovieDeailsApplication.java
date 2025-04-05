08b4c9ed0abed0716d4bade6560373f6795d2225
Liliána Bohus <boli96@gmail.com>
2025-04-05 13:37:05 +0200

    feat: initial commit - spring boot setup

---
+++ src/main/java/hu/informula/movie_deails/MovieDeailsApplication.java
@@ -0,0 +1,13 @@
+package hu.informula.movie_deails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDeailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDeailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/informula/movie_deails/MovieDeailsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.informula.movie_deails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDeailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
